c8f0aa8ab22a0ccf58d249c2ed04ae150fcc6201
Adika2003 <adikasultana88m@gmail.com>
2025-10-04 18:01:30 +0600

    20 commit

--- mysite/clubs/models.py
+++ mysite/clubs/models.py
@@ -1,11 +1,18 @@
+from typing import Any
 from django.db import models
 from django.urls import reverse
+
 
 class Club(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
     logo = models.ImageField(upload_to='club_logos/', blank=True, null=True)
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.events = None
+        self.members = None
 
     def __str__(self):
         return self.name
