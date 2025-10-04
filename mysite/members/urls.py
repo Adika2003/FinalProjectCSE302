2947f09adeffbec861a794fdb5d2f12d6328c3c3
Adika2003 <adikasultana88m@gmail.com>
2025-10-04 18:02:20 +0600

    24 commit

--- mysite/members/urls.py
+++ mysite/members/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'members'
 
 urlpatterns = [
-    path('', views.member_list, name='list'),
+    path('<slug:club_slug>/', views.member_list, name='list'),
+    path('update/<int:pk>/', views.member_update, name='update'),
 ]
