34672b3ffda23936687fa8845f2e48ae41694cfe
Adika2003 <adikasultana88m@gmail.com>
2025-10-04 13:41:58 +0600

    Eleven commit

--- mysite/clubs/urls.py
+++ mysite/clubs/urls.py
@@ -5,7 +5,6 @@ app_name = 'clubs'
 
 urlpatterns = [
     path('', views.club_list, name='list'),
-    path('create/', views.club_create, name='create'),
+
     path('<slug:slug>/', views.club_detail, name='detail'),
 ]
-
