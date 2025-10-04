135dfb0313b6c4f51616f9ec05ddc8db3d7bfdf9
Adika2003 <adikasultana88m@gmail.com>
2025-10-04 23:09:57 +0600

    28 commit

--- mysite/members/views.py
+++ mysite/members/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Member
-from Clubs.models import Club
+from clubs.models import Club
 from .forms import MemberForm
 
 
@@ -9,14 +9,10 @@ from .forms import MemberForm
 def member_list(request, club_slug):
     club = get_object_or_404(Club, slug=club_slug)
 
-    # Ensure 'members' related_name exists on Member model
-    members_qs = getattr(club, 'members', None)
-    if members_qs is not None:
-        convener = club.members.filter(role_type='convener').first()
-        executives = club.members.filter(role_type='executive')
-        generals = club.members.filter(role_type='general')
-    else:
-        convener = executives = generals = None
+    members = Member.objects.filter(club=club)
+    convener = members.filter(role_type='convener').first()
+    executives = members.filter(role_type='executive')
+    generals = members.filter(role_type='general')
 
     context = {
         'club': club,
@@ -35,14 +31,10 @@ def member_update(request, pk):
     if request.method == 'POST':
         form = MemberForm(request.POST, request.FILES, instance=member)
         if form.is_valid():
-            updated_member = form.save(commit=False)
-            updated_member.save()
-
-            # Redirect to member list of that club if exists
+            updated_member = form.save()
             if updated_member.club:
-                return redirect('members:list', club_slug=updated_member.club.slug)
-            else:
-                return redirect('clubs:list')
+                return redirect('member_list', club_slug=updated_member.club.slug)
+            return redirect('club_list')
     else:
         form = MemberForm(instance=member)
 
